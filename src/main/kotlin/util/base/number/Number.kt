5bdbee5dd1528f222ad5de02436cc1e447749a04
rrain <rrainpath@gmail.com>
2025-08-09 10:36:32 +0800

    refactor

--- src/main/kotlin/util/base/number/Number.kt
+++ src/main/kotlin/util/base/number/Number.kt
@@ -2,4 +2,4 @@ package com.rrain.util.base.number
 
 
 
-fun Int.mapZero(block: () -> Int): Int = if (this == 0) block() else this
+inline fun Int.mapZero(block: () -> Int): Int = if (this == 0) block() else this
